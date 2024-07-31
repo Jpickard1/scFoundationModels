9cd972e9c4a3869261279a2605f2009c1544de06
Joshua <jpic@umich.edu>
2024-07-31 02:05:38 -0400

    nat emailed a script and I tried submitting it out of the box

--- HSC/geneformerHSCembeddings.py
+++ HSC/geneformerHSCembeddings.py
@@ -11,7 +11,10 @@ TODO:
    to save the perturbed embeddings.
 3. It looks to me as if the perturbations occur on line 220 but the embeddings have already occured on line 191. I 
    am not the most familiar with this code, but it does seem unusual/backwards to me.
+
+NOTE: DONE< 7/30
 """
+
 import sys
 import seaborn as sns
 import pandas as pd 
@@ -30,7 +33,6 @@ from importlib import reload
 from datasets import Dataset, load_from_disk
 from datasets import load_dataset
 from geneformer import EmbExtractor
-import geneformer as gtu
 
 # classifer tools
 import xgboost
@@ -39,31 +41,93 @@ from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import confusion_matrix
 
-# local imports
-sys.path.insert(0, '../../scripts/')
-import geneformer_utils as gtu
+
+import pandas as pd 
+import numpy as np
+import anndata as an
+import scanpy as sc
+import pickle
+
+from datasets import Dataset, load_from_disk, load_dataset
+import geneformer
+
+from datetime import datetime
+
+DEFAULT_NAME_PATH = "/nfs/turbo/umms-indikar/shared/projects/geneformer/geneformer/gene_name_id_dict.pkl"
+DEFAULT_TOKEN_PATH = "/nfs/turbo/umms-indikar/shared/projects/geneformer/token_dictionary.pkl"
+DEFAULT_MEDIAN_PATH = "/nfs/turbo/umms-indikar/shared/projects/geneformer/geneformer/gene_median_dictionary.pkl"
 
 sns.set_style('white')
 torch.cuda.empty_cache()
 
+
+
+### Parameters v
+###
+
+# cells to filter on
+initial_cell_type = 'Fibroblast'
+
+# list of genes to perturb with at the front of the list
+gene_list = [
+    'GATA2', 
+    'GFI1B', 
+    'FOS', 
+    'STAT5A',
+    'REL',
+    'FOSB',
+    'IKZF1',
+    'RUNX3',
+    'MEF2C',
+    'ETV6',
+]
+
+############### important! ###############
+# added back sampling, now all cells
+#num_initial_cells = 10
+################  (1/2)  #################
+MODEL_PATH = "/scratch/indikar_root/indikar1/shared_data/geneformer/fine_tune/240715_geneformer_cellClassifier_no_induced/ksplit1/"
+#     Args:
+#         MODEL_PATH (str): Path to the model file.
+#         model_type (str, optional): Type of model ('Pretrained' or custom). Default: 'Pretrained'.
+#         n_classes (int, optional): Number of output classes for custom models. Default: 0.
+#         mode (str, optional): Mode to load the model in ('eval' or 'train'). Default: 'eval'.
+model = geneformer.perturber_utils.load_model('Pretrained', 0 , MODEL_PATH, 'eval')
+
+
+print('model loaded!')
+
+
+TOKEN_DATA_PATH = "/scratch/indikar_root/indikar1/shared_data/geneformer/resources/token_mapping.csv"
+token_df = pd.read_csv(TOKEN_DATA_PATH)
+
+
+DATA_PATH = "/scratch/indikar_root/indikar1/shared_data/geneformer/fine_tune/hsc.dataset"
+
+
+
+###
+### Parameters ^
+
+
 # Uses token_df to translate from gene_list to tokens_list v
 def get_tokens_list(gene_list):
-    """
-    TODO: <Please write doc strings to describe the inputs and outputs of this method>
-    """
     # Get a df of the genes we are perturbing with
     genes = token_df[token_df['gene_name'].isin(gene_list)]
-
+    
     tf_map = dict(zip(genes['gene_name'].values, genes['token_id'].values))
-
+    
     # Create tokens_list by looking up each gene_name in the tf_map
     tokens_list = [tf_map.get(gene_name, gene_name) for gene_name in gene_list]
 
     return tokens_list
 
+
 def add_perturbations_to_cell(cell_tokens, perturbation_tokens):
     """
-    TODO: <Please write doc strings to describe the inputs and outputs of this method>
+    Requires: cell_tokens is a list of (in our use, 2048) integer tokens, each token representing a gene,
+        perturbation_tokens is a list of integer tokens
+    Effects: returns final_tokens, a list of tokens with perturbation_tokens at the front
     """
     original_length = len(cell_tokens)
 
@@ -76,79 +140,129 @@ def add_perturbations_to_cell(cell_tokens, perturbation_tokens):
 
     return final_tokens
 
-def main():
+def extract_embedding_in_mem(model, data, emb_mode='cell', layer_to_quant=-1, forward_batch_size=10):
+    """Extracts embeddings from a model and returns them as a DataFrame.
+
+    This function provides an in-memory extraction of embeddings, allowing for convenient
+    manipulation and analysis directly within your Python environment.
+
+    Args:
+        model: The model to use for embedding extraction.
+        data: The input data for which embeddings need to be extracted.
+        emb_mode (str, optional): The embedding mode. Defaults to 'cell'.
+        layer_to_quant (int, optional): The layer to quantize. Defaults to -1 (last layer).
+        forward_batch_size (int, optional): The batch size for forward passes. Defaults to 10.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the extracted embeddings.
+
+    Raises:
+        TypeError: If `model` is not a supported model type.
+        ValueError: If `data` is not in the correct format.
     """
-    TODO: <Please write doc strings to describe the inputs and outputs of this method and how it works>
+
+    embs = geneformer.emb_extractor.get_embs(
+        model,
+        data,
+        emb_mode,
+        layer_to_quant,
+        0,  # Assuming this is a constant parameter for the function
+        forward_batch_size,
+        summary_stat=None,  
+        silent=False, 
+    )
+    data = embs.cpu().numpy()
+    if emb_mode=='cell':
+        return pd.DataFrame(data)
+    else:
+        return data
+
+
+def embedding_to_adata(df: pd.DataFrame, n_dim: int = None) -> an.AnnData:
+    """Converts a Pandas DataFrame with an embedding to an AnnData object.
+
+    Args:
+        df: The input DataFrame with numerical embedding columns and optional metadata columns.
+        n_dim: The number of dimensions to keep in the embedding. If None, all dimensions are kept.
+
+    Returns:
+        The converted AnnData object.
+
+    Raises:
+        ValueError: If `n_dim` exceeds the available dimensions in the DataFrame.
     """
-    ###########################################################################
-    ### Parameters v
-    ###
-    ###    TODO: Please explain what goes on in this sections. I know it looks
-    ###          super simple, but pretend someone who knows less than you will
-    ###          be responsible for running and modifying this code in the
-    ###          future.
 
-    jobNumber = sys.argv[1]
+    if n_dim is not None and n_dim > df.shape[1]:
+        raise ValueError(f"n_dim ({n_dim}) exceeds available dimensions ({df.shape[1]})")
 
-    # TODO: Get list of genes for the specific job number
-    # perturbations = pd.read_csv(we can make a file of all the perturbations)
-    # gene_list = perturbations[:, jobNum]
+    # Assuming embedding columns are those that are not integers
+    is_metadata = df.columns.astype(str).str.isdigit()
+    metadata_df = df.loc[:, ~is_metadata]
+    embedding_df = df.loc[:, is_metadata]
 
-    # cells to filter on
-    initial_cell_type = 'Fibroblast'
+    cell_index = pd.Index([f"C{x}" for x in range(df.shape[0])], name='obs_names')
 
-    # list of genes to perturb with at the front of the list
-    # TODO: remove this list of genes once the above TODO is complete
+    if n_dim is not None:
+        embedding_df = embedding_df.iloc[:, :n_dim]
+
+    var_index = pd.Index([f"D{x}" for x in range(embedding_df.shape[1])], name='var_names')
+
+    adata = an.AnnData(embedding_df.to_numpy())
+    adata.obs_names = cell_index
+    adata.var_names = var_index
+    adata.obs = metadata_df
+    return adata
+
+
+##########
+"""
+FOR OUR USE CASE SPECIFICALLY.
+"""
+
+def ten_choose_five():
     gene_list = [
-        'GATA2', 
-        'GFI1B', 
-        'FOS', 
-        'STAT5A',
-        'REL',
-        'FOSB',
-        'IKZF1',
-        'RUNX3',
-        'MEF2C',
-        'ETV6',
-    ]
+    'GATA2', 
+    'GFI1B', 
+    'FOS', 
+    'STAT5A',
+    'REL',
+    'FOSB',
+    'IKZF1',
+    'RUNX3',
+    'MEF2C',
+    'ETV6',
+]
+# Define the length of sublists
+    len_sublist = 5
     
-    model_path = "/scratch/indikar_root/indikar1/shared_data/geneformer/fine_tune/240715_geneformer_cellClassifier_no_induced/ksplit1/"
-
-    token_data_path = "/scratch/indikar_root/indikar1/shared_data/geneformer/resources/token_mapping.csv"
-
-    data_path = "/scratch/indikar_root/indikar1/shared_data/geneformer/fine_tune/hsc.dataset"
-
-    ###
-    ### Parameters ^
-
-    ###########################################################################
-
-    ### Preliminaries v
-    ###
-    ###    TODO: Please explain what goes on in this sections.
-
-    if torch.cuda.is_available(): 
-        print("CUDA is available! Devices: ", torch.cuda.device_count()) 
-        print("Current CUDA device: ", torch.cuda.current_device()) 
-        print("Device name: ", torch.cuda.get_device_name(torch.cuda.current_device())) 
-    else: print("CUDA is not available")
-
-    model = gtu.load_model(model_path)
-    print('model loaded!')
-
-    token_df = pd.read_csv(token_data_path)
-    token_df.head()
-
-    ###
-    ### Preliminaries ^
-
-    ###########################################################################
+    # Generate all combinations of the specified length
+    sublists = list(combinations(gene_list, len_sublist))
     
+    # Create the DataFrame
+    df = pd.DataFrame({
+        'recipe_iteration': range(1, len(sublists) + 1),
+        'recipe_list': [list(sublist) for sublist in sublists]
+    })
+    return df
+
+# Check and ensure all data is of correct type
+def check_and_convert(data):
+    if isinstance(data, pd.DataFrame):
+        for col in data.columns:
+            if not pd.api.types.is_string_dtype(data[col]):
+                data[col] = data[col].astype(str)
+    return data
+
+    
+#######
+
+    
+def single_recipe(recipe_list):
     ### Format raw data, print messages to check v
     ###
 
     # Load from pre-trained data
-    raw_data = load_from_disk(data_path)
+    raw_data = load_from_disk(DATA_PATH)
 
     # Convert to DataFrame for filtering
     df = raw_data.to_pandas()
@@ -159,12 +273,18 @@ def main():
     # Filtering
     fb_df = df[df['standardized_cell_type'] == initial_cell_type]
 
+    ############### important! ###############
+    # sampling (ADDED BACK!)
+    #fb_df = fb_df.sample(num_initial_cells)
+    ################  (2/2)  #################
+
+    fb_df = fb_df.reset_index(drop=True)
+
     # add a cell id
     fb_df['cell_id'] = [f"cell_{i+1}" for i in range(len(fb_df))]
     fb_df['recipe'] = 'raw'  # as opposed to having a speciofic ;-separated recipe list. other entries will have this.
     fb_df['type'] = 'initial' # this dataframe no longer includes 'target'
 
-    # display some information about the dataset
     print("\nFiltered Dataset:")
     print(f"  - Number of samples: {fb_df.shape[0]:,}")   # Nicer formatting with commas
     print(f"  - Number of columns: {fb_df.shape[1]:,}")
@@ -173,40 +293,15 @@ def main():
     print("\nCell Type Distribution (Filtered):")
     print(fb_df['standardized_cell_type'].value_counts().sort_index())  # Sort for readability
 
-    # Construct the dataset
+    # Convert back to Dataset
     fb_data = Dataset.from_pandas(fb_df)
     print(f"\nDataset converted back: {fb_data}")
 
     ###
     ### Format raw data, print messages to check ^
 
-    ###########################################################################
-
-    ### Get the embeddings into an Anndata object v
-    ###
-
-    reload(gtu) # TODO: I think we should delete this line - JP
-    torch.cuda.empty_cache()
-
-    # Construct the embeddings of the dataset
-    fb_embs = gtu.extract_embedding_in_mem(
-        model, 
-        fb_data, 
-        layer_to_quant=-1,
-        forward_batch_size=100,
-    )
-    print(f"{fb_embs.shape=}")
-
-    # translate into an anndata object
-    fb_adata = gtu.embedding_to_adata(fb_embs)
-    fb_adata.obs = fb_df.copy()
-    fb_adata.obs.head() # TODO: I think we could delete this line - JP
-
-    ###
-    ### Get the embeddings into an Anndata object ^
-
-    ###########################################################################
-
+    ############################################################################
+    
     ### Perform the perturbation v
     ###
 
@@ -215,24 +310,82 @@ def main():
     ]
 
     perturb = fb_df.copy()
-    recipe = ";".join(gene_list)
+    recipe = ";".join(recipe_list)
     perturb['recipe'] = recipe
     perturb['type'] = 'reprogrammed'
-    perturb['input_ids'] = perturb['input_ids'].apply(lambda x: add_perturbations_to_cell(x, get_tokens_list(gene_list)))
+    perturb['input_ids'] = perturb['input_ids'].apply(lambda x: add_perturbations_to_cell(x, get_tokens_list(recipe_list)))
 
     reprogramming_df.append(perturb)
 
     reprogramming_df = pd.concat(reprogramming_df, ignore_index=True)
 
     print(f"{reprogramming_df.shape=}")
-    reprogramming_df.sample(10) # TODO: I think we should delete this line - JP
 
     ###
     ### Perform the perturbation ^
 
-    ###########################################################################
+    ############################################################################
+    ### Get the embeddings into an Anndata object v
+    ###
 
-    # TODO: Save the reprogrammed dataframe to a file.
+    torch.cuda.empty_cache()
+
+    reprogramming_data = Dataset.from_pandas(reprogramming_df)
+
+    reprogramming_embs = extract_embedding_in_mem(
+        model, 
+        reprogramming_data, 
+        layer_to_quant=-1,
+        forward_batch_size=100,
+    )
+    print(f"{reprogramming_embs.shape=}")
+
+    # translate into an anndata object and plot
+    reprogramming_adata = embedding_to_adata(reprogramming_embs)
+    reprogramming_adata.obs = reprogramming_df.copy()
+
+
+    reprogramming_adata.obs.head()
+
+    return reprogramming_adata
+
+
+
+def main():
+
+    jobNumber = int(sys.argv[1])
+    print(jobNumber)
+
+
+    # For our iterations specifically...
+    all_input_recipes = ten_choose_five()
+    recipe_list = all_input_recipes.at[all_input_recipes[all_input_recipes['recipe_iteration'] == jobNumber].index[0], 'recipe_list']
+
+
+    if torch.cuda.is_available(): 
+        print("CUDA is available! Devices: ", torch.cuda.device_count()) 
+        print("Current CUDA device: ", torch.cuda.current_device()) 
+        print("Device name: ", torch.cuda.get_device_name(torch.cuda.current_device())) 
+    else: print("CUDA is not available")
+
+    # run on a specific perturbation ( last thing not yet: change with ten_choose_five() )
+    reprogramming_adata = single_recipe(recipe_list)
+
+
+    reprogramming_adata.obs = check_and_convert(reprogramming_adata.obs)
+    reprogramming_adata.var = check_and_convert(reprogramming_adata.var)
+   
+        
+    filepath = f"/nfs/turbo/umms-indikar/shared/projects/geneformer/fib15k/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_job_number_{jobNumber}.h5ad"
+
+    try:
+        reprogramming_adata.write(filepath)
+        print(f"File successfully written to {filepath}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    
+
+
 
 
 
--- HSC/geneformerHSCembeddings.sh
+++ HSC/geneformerHSCembeddings.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --job-name=GF-HSC
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=oliven@umich.edu
+#SBATCH --mail-user=jpic@umich.edu
 #SBATCH --nodes=1
 #SBATCH --mem-per-gpu=180g
-#SBATCH --time=10:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=indikar0
 #SBATCH --partition=gpu,spgpu,gpu_mig40
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-task=8
 #SBATCH --gpus=1
-#SBATCH --output=/home/oliven/%x-%j.log
+#SBATCH --output=/home/jpic/HSC-logs/%x-%j.log
+#SBATCH --array=1-252
 
 ### name this something like: geneformerHSCembeddings.sh
 ###    good practice is to pair the name of this bash script with the name of the python file it will run
@@ -22,4 +22,4 @@
 ### ```
 
 # run the script
-python geneformerHSCembeddings.py
+python geneformerHSCembeddings.py $SLURM_ARRAY_TASK_ID
